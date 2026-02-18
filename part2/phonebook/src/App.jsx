6cca8b05e0096a5b2a94f5618a6f2f853e74f437
Aldana <aherbel@frsf.utn.edu.ar>
2026-02-17 21:57:07 -0300

    exercise 2.11 part2 phonebook added

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,21 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
 
 const App = () => {
   
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
   const [personsSearch, setPersonsSearch] = useState([])
   const [all, setAll] = useState(true)
+
+  // recuperar información del servidor json
+  const hook = () => {
+    axios.get('http://localhost:3001/persons').then(response => {setPersons(response.data)})
+  }
+
+  useEffect(hook,[])
 
   // agregar persona
   const addPerson = (event) => {
